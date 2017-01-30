a5a8d6411be1ce9e278c811b5ceed226e2397d07
Sören Plönnigs <ploennigs@tet.uni-hannover.de>
2017-01-30 11:28:00 +0100

    modified:   ArchMaster 	modified:   custom/TET 	modified:   samplePackageConfigurations/base_pkg_official.txt 	modified:   samplePackageConfigurations/gnome_environment.txt 	modified:   scripts/setupAasEnv.sh

--- scripts/setupAasEnv.sh
+++ scripts/setupAasEnv.sh
@@ -17,23 +17,56 @@ SysConfDir=/etc/aas
 SysConfFile=$SysConfDir/config
 CustomScriptDir=$SysConfDir/custom
 
-# create SysConfDir
+# create global configuration directory
 [ ! -d $SysConfDir ] && {
-  infoMessage -n "Creating directory $SysConfDir ... "
-  mkdir -p $SysConfDir
-  [ -d $SysConfDir ] && okMessage "OK" || error "Failed"
+	infoMessage -n "Creating directory $SysConfDir ... "
+	sudo mkdir $SysConfDir
+	[ ! -d $SysConfDir ] && error "Could not create directory $SysConfDir" || okMessage "OK"
 }
 
-# create CustomScriptDir
-[ ! -d $CustomScriptDir ] && {
-  infoMessage -n "Creating directory $CustomScriptDir ... "
-  mkdir -p $CustomScriptDir
-  [ -d $CustomScriptDir ] && okMessage "OK" || error "Failed"
-}
-
-# create main configuration file
+# create global configuration file
 [ ! -f $SysConfFile ] && {
-  infoMessage -n "Creating file $SysConfFile ... "
-  touch $SysConfFile
-  [ -f $SysConfFile ] && okMessage "OK" || error "Failed"
+	infoMessage -n "Creating file `readlink -m $SysConfFile` ... "
+	sudo touch $SysConfFile
+	[ ! -f $SysConfFile ] && error "Failed to create file `readlink -m $SysConfFile`" || okMessage "OK"
+}
+
+# create custom scripts directory
+[ ! -d $CustomScriptDir ] && {
+	infoMessage -n "Creating directory $CustomScriptDir ... "
+	sudo mkdir $CustomScriptDir
+	[ ! -d $CustomScriptDir ] && error "Could not create directory $CustomScriptDir" || okMessage "OK"
+}
+
+# add self update option
+[ -z "`grep 'SELF_UPDATE=true' $SysConfFile`" ] && {
+  infoMessage "Adding self update option to AAS config file"
+  echo "# run self update" | sudo tee --append $SysConfFile
+  echo "SELF_UPDATE=true" | sudo tee --append $SysConfFile
+}
+
+# User environment
+
+# create ~/.bashrc
+UserBashCfg=~/.bashrc
+[ ! -f $UserBashCfg ] && {
+	sysMessage -n "Creating file `readlink -m $UserBashCfg` ... "
+	touch $UserBashCfg
+	[ ! -f $UserBashCfg ] && error "Failed to create file `readlink -m $UserBashCfg`" || okMessage "OK"
+}
+
+# modify ~/.bashrc
+PathStr=~/.aas
+[ -z "`cat $UserBashCfg | grep PATH=`" ] && {
+	sysMessage -n "Adding PATH defintion to bash configuration file $UserBashCfg ... "
+	echo "export PATH=\$PATH:$PathStr" > $UserBashCfg
+	[ -z "`cat $UserBashCfg | grep PATH`" ] && error "Failed" || okMessage "OK"
+} || {
+	[ -z "`cat $UserBashCfg | grep $PathStr`" ] && {
+		sysMessage -n "Adding $PathStr to user PATH ... "
+		OldPathStr=`cat $UserBashCfg | grep PATH=`
+		NewPathStr="$OldPathStr:$PathStr"
+		sed "s|$OldPathStr|$NewPathStr|g" -i $UserBashCfg
+		[ -z "`cat $UserBashCfg | grep $PathStr`" ] && error "Failed" || okMessage "OK"
+	}
 }
